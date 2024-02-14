248f7c31be03635df3c509b582cb569843dab70f
pazhanivel <palani_a@rediffmail.com>
2024-02-14 18:35:57 +0530

    Python Test file

---
+++ Test_python.py
@@ -0,0 +1,18 @@
+print("Hello World")
+my_print = print
+my_print('hello from my_print!')c
+x = 5.62
+print(type(x))
+
+
+d = 6
+e = 2
+
+print(d ** e)
+
+d += e
+print (d)
+
+pi = 3.1415911111111122211111111111111111111;
+print("Pi =", pi)
+print("or", 3.14, "for short")
